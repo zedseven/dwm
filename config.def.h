ae5d672aff7ebde27d3c337d67b1cca32b4df625
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2023-11-28 23:47:52 -0500

    Fix the missing declaration of `tatami`.

--- config.def.h
+++ config.def.h
@@ -42,6 +42,7 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "|+|",      tatami },
 };
 
 /* key definitions */
--- dwm.c
+++ dwm.c
@@ -209,6 +209,7 @@ static void spawn(const Arg *arg);
 static void tag(const Arg *arg);
 static void tagmon(const Arg *arg);
 static void tile(Monitor *m);
+static void tatami(Monitor *m);
 static void togglebar(const Arg *arg);
 static void togglefloating(const Arg *arg);
 static void toggletag(const Arg *arg);
